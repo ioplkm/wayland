a2c0bd32aaf90fc4251799a1044826e4060dc7e5
ioplkm <ioplkm352@gmail.com>
2022-08-08 19:56:10 +0500

    save

--- client.c
+++ client.c
@@ -23,6 +23,7 @@ struct wl_surface *wl_surface;
 struct xdg_wm_base *xdg_wm_base;
 struct xdg_surface *xdg_surface;
 struct xdg_toplevel *xdg_toplevel;
+struct wl_callback *callback;
 
 void bufRelease(void *data, struct wl_buffer *wl_buffer){
     wl_buffer_destroy(wl_buffer);
@@ -30,11 +31,28 @@ void bufRelease(void *data, struct wl_buffer *wl_buffer){
 
 struct wl_buffer_listener bufListener = {bufRelease};
 
-void draw_frame();
+int c = 0;
+struct wl_buffer *draw() {
+  fd = allocate_shm_file(size);
+  pool = wl_shm_create_pool(wl_shm, fd, size);
+  data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    for (int i = 0; i < height * width; i++) {
+      data[i] = c++;
+    }
+  munmap(data, size);
+  buffer = wl_shm_pool_create_buffer(pool, 0, width, height, stride, WL_SHM_FORMAT_XRGB8888);
+  wl_shm_pool_destroy(pool);
+  close(fd);
+  wl_buffer_add_listener(buffer, &bufListener, NULL);
+  return buffer;
+}
 
 void surfaceConf(void *data, struct xdg_surface *xdg_surface, uint32_t serial) {
   xdg_surface_ack_configure(xdg_surface, serial);
-  draw_frame();
+  buffer = draw();
+  
+  wl_surface_attach(wl_surface, buffer, 0, 0);
+  wl_surface_commit(wl_surface);
 }
 
 struct xdg_surface_listener surfaceListener = {surfaceConf};
@@ -60,14 +78,22 @@ void regDel(void *data, struct wl_registry *wl_registry, uint32_t name) {
 
 }
 
-static const struct wl_registry_listener regListener = {regAdd, regDel};
+struct wl_registry_listener regListener = {regAdd, regDel};
 
-void draw_frame() {
-  buffer = wl_shm_pool_create_buffer(pool, 0, width, height, stride, WL_SHM_FORMAT_XRGB8888);
-  wl_buffer_add_listener(buffer, &bufListener, NULL);
+struct wl_callback_listener surfaceFrameListener;
+
+void surfaceCallback(void *data, struct wl_callback *callback, uint32_t time) {
+  wl_callback_destroy(callback);
+  callback = wl_surface_frame(wl_surface);
+  wl_callback_add_listener(callback, &surfaceFrameListener, NULL);
+
+  buffer = draw();
   wl_surface_attach(wl_surface, buffer, 0, 0);
+  wl_surface_damage_buffer(wl_surface, 0, 0, INT32_MAX, INT32_MAX);
   wl_surface_commit(wl_surface);
-}
+};
+
+struct wl_callback_listener surfaceFrameListener = {surfaceCallback};
 
 void init() {
   wl_display = wl_display_connect(NULL);
@@ -78,18 +104,15 @@ void init() {
   wl_surface = wl_compositor_create_surface(wl_compositor);
   xdg_surface = xdg_wm_base_get_xdg_surface(xdg_wm_base, wl_surface);
   xdg_surface_add_listener(xdg_surface, &surfaceListener, NULL);
-
-  fd = allocate_shm_file(size);
-  pool = wl_shm_create_pool(wl_shm, fd, size);
-  data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
   xdg_toplevel = xdg_surface_get_toplevel(xdg_surface);
   wl_surface_commit(wl_surface);
+
+  callback = wl_surface_frame(wl_surface);
+  wl_callback_add_listener(callback, &surfaceFrameListener, NULL);
 }
 
 int main() {
   init();
-  for (int i = 0; i < height * width; i++) 
-    data[i] = i*999;
   while (wl_display_dispatch(wl_display)) {
   }
   return 0;
